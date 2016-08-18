9f60bb53d6ef25d8f3623b2f791bc3844778ccad
hongyi liu <wenhualiu.liu@gmail.com>
2016-08-18 15:58:31 -0400

    Initial Commit

---
+++ LetFun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LetFun
+//
+//  Created by hongyi liu on 8/18/16.
+//  Copyright © 2016 hongyi liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
